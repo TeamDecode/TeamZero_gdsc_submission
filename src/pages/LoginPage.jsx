9f69c2f1fd75e6099156b55d47c712b680fefa2e
TeamDecode <146320330+TeamDecode@users.noreply.github.com>
2024-02-09 19:39:56 +0530

    nab

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,9 @@
 import Popup from "../components/PageAccessories/Popup"
 import { useState } from 'react';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faHippo, faLock } from "@fortawesome/free-solid-svg-icons";
+import "./LoginPage.css"
 
 const LoginPage = () => {
     const [email, setEmail] = useState('');
@@ -30,21 +33,24 @@ const LoginPage = () => {
     };
 
   return (
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700 pb-7">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-              <h2 className="font-title mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-300">
-                  Login to your account
+    <body>
+      <div className="body">
+          <div className="login">
+          <div className="">
+              <h2 className="login-text">
+                  Login To Your account
               </h2>
           </div>
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm"> {/* paddding */}
               <form className="space-y-6" onSubmit={validateEverything}>
                   <div>
-                      <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-400">
-                          Email address
+                      <label htmlFor="email" className=" text-xl/ block text-sm font-medium leading-6 ">
+                      <FontAwesomeIcon icon={faEnvelope} /> Email address
                       </label>
+                      
                       <div className="mt-2">
+                      
                           <input
                               id="email"
                               name="email"
@@ -54,18 +60,19 @@ const LoginPage = () => {
                               autoComplete="email"
                               placeholder="Type your Email ID"
                               required
-                                  className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                          />
+                                  className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 italic"
+                          ></input>
                       </div>
                   </div>
 
                   <div>
                       <div className="flex items-center justify-between">
-                          <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-400" >
-                              Password
+                          <label htmlFor="password" className="block text-sm font-medium leading-6 " >
+                          <FontAwesomeIcon icon={faLock} />  Password
                           </label>
                       </div>
                       <div className="mt-2">
+                     
                           <input
                               id="password"
                               name="password"
@@ -75,7 +82,7 @@ const LoginPage = () => {
                               placeholder="Type your Password"
                               autoComplete="current-password"
                               required
-                                  className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                                  className=" italic border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                           />
                       </div>
                   </div>
@@ -97,6 +104,8 @@ const LoginPage = () => {
           {showPopup && <Popup alertTitle="Invalid" alertText="Check if the email ID is valid and both the passwords are same!" />}
           </div>
       </div>
+       
+       </body>
   )
 }
 
